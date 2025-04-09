8ba103d9b983996a9497d7482466e2fc50c4318f
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2025-04-09 07:37:31 +0100

    Adjusted to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { GameProvider } from './context/DataContext.tsx'
 import Navbar from './components/Navbar.tsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import {DetailsPage,Store,Search, NotFound} from './Pages'
 import './fonts/GTWalsheimPro/stylesheet.css'
 
@@ -16,10 +16,10 @@ createRoot(document.getElementById('root')).render(
         <Router basename='/GameshopV2'>
           <Navbar />
           <Routes>
-            <Route path='#/' element={<App />} />
-            <Route path='#/details/:id' element={<DetailsPage />} />
-            <Route path='#/store' element={<Store />} />
-            <Route path='#/search/:name' element={<Search />} />
+            <Route path='/' element={<App />} />
+            <Route path='/details/:id' element={<DetailsPage />} />
+            <Route path='/store' element={<Store />} />
+            <Route path='/search/:name' element={<Search />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
         </Router>
